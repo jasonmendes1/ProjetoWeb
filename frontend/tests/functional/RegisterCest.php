cba6eee84438adc7c9026a5a88b4d2ad600c8c9e
Jason Mendes <jason_mendes_1998@hotmail.com>
2020-12-28 15:51:34 +0000

    Functional Tests

---
+++ frontend/tests/functional/RegisterCest.php
@@ -0,0 +1,50 @@
+<?php namespace frontend\tests\functional;
+use frontend\tests\FunctionalTester;
+
+class LoginCest
+{
+    public function tryCreateUser(FunctionalTester $I)
+    {
+        // Navegação desde Index até Registo
+        $I->amOnPage(\yii::$app->homeUrl);
+        $I->click('Convidado');
+        $I->click('Registar');
+
+        // Inserção de Dados
+        $I->fillField('Username', 'userteste');
+        $I->fillField('Email', 'emailteste@emailteste.com');
+        $I->fillField('Password', 'passwordteste');
+        //$I->click('SignupForm[avatar]');
+       // $I->fillField('avatar', 'web/images/test.png');
+        $I->attachFile('#signupform-avatar', 'logo.png');
+        $I->fillField('Primeiro Nome', 'NomeTeste');
+        $I->fillField('Apelido', 'ApelidoTeste');
+        $I->fillField('Dt Nascimento', '1990-01-01');
+
+        $option = $I->grabTextFrom('select option:nth-child(2)');
+        $I->selectOption("select", $option);
+
+        $I->fillField('Num Tele', '912345678');
+        $I->fillField('Nif', '123456789');
+
+        //Final Create
+        $I->click('signup-button');
+        $I->see('Thank you for registration. Please check your inbox for verification email.');
+
+
+
+        // Login User
+        $I->amOnPage(\yii::$app->homeUrl);
+        $I->click('Registar/Login');
+        $I->click('Fazer Login');
+        $I->see('Please fill out the following fields to login:');
+        $I->fillField('Username', 'userteste');
+        $I->fillField('Password', 'passwordteste');
+        $I->see('Remember Me');
+        $I->click('Login');
+        $I->see('Fitness League');
+        $I->click('Registar/Login');
+        $I->click( '.logout');
+    }
+
+}
