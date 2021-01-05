1c1aa65da9216700391d8591e12dbd7fc63e8fb1
Jason Mendes <jason_mendes_1998@hotmail.com>
2021-01-05 16:51:36 +0000

    EmentaTest Unitario

---
+++ backend/tests/unit/EmentaTest.php
@@ -0,0 +1,98 @@
+<?php namespace backend\tests;
+
+use common\models\Ementa;
+
+class EmentaTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \backend\tests\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testValidation()
+    {
+        $ementa = new Ementa();
+        
+        $ementa->PequenoAlmoco = null;
+        $this->assertFalse($ementa->validate(['PequenoAlmoco']));
+        $ementa->PequenoAlmoco = 'fghnvghtoolooooongndsdsadsadsadsadsadsadsadsazdsfsdfdsgfdgsgsbrsgbsbgsfersgsfdgsdsadsadsadsdsadsadsadsadsadasfbfasefebsabfasbfabdsfsfbebaesbfasefbasebfaesfbaesfesbfaesfbasdsadasdadasdsadasafghnvghtoolooooongnaaaaaaameeeefghjkcxdftyhfghjkcxdfghjkcxhsshfgvhjvgjhgjhgjhgjhgj';
+        $this->assertFalse($ementa->validate(['PequenoAlmoco']));
+        $ementa->PequenoAlmoco = 'PequenoAlmoco';
+        $this->assertTrue($ementa->validate(['PequenoAlmoco']));
+
+        $ementa->Almoco = null;
+        $this->assertFalse($ementa->validate(['Almoco']));
+        $ementa->Almoco = 'fghnvghtoolooooongndsdsadsadsadsadsadsadsadsazdsfsdfdsgfdgsgsbrsgbsbgsfersgsfdgsdsadsadsadsdsadsadsadsadsadasfbfasefebsabfasbfabdsfsfbebaesbfasefbasebfaesfbaesfesbfaesfbasdsadasdadasdsadasafghnvghtoolooooongnaaaaaaameeeefghjkcxdftyhfghjkcxdfghjkcxhsshfgvhjvgjhgjhgjhgjhgj';
+        $this->assertFalse($ementa->validate(['Almoco']));
+        $ementa->Almoco = 'Almoco';
+        $this->assertTrue($ementa->validate(['Almoco']));
+
+        $ementa->Lanche1 = null;
+        $this->assertFalse($ementa->validate(['Lanche1']));
+        $ementa->Lanche1 = 'fghnvghtoolooooongndsdsadsadsadsadsadsadsadsazdsfsdfdsgfdgsgsbrsgbsbgsfersgsfdgsdsadsadsadsdsadsadsadsadsadasfbfasefebsabfasbfabdsfsfbebaesbfasefbasebfaesfbaesfesbfaesfbasdsadasdadasdsadasafghnvghtoolooooongnaaaaaaameeeefghjkcxdftyhfghjkcxdfghjkcxhsshfgvhjvgjhgjhgjhgjhgj';
+        $this->assertFalse($ementa->validate(['Lanche1']));
+        $ementa->Lanche1 = 'Lanche1';
+        $this->assertTrue($ementa->validate(['Lanche1']));
+
+        $ementa->Lanche2 = null;
+        $this->assertFalse($ementa->validate(['Lanche2']));
+        $ementa->Lanche2 = 'fghnvghtoolooooongndsdsadsadsadsadsadsadsadsazdsfsdfdsgfdgsgsbrsgbsbgsfersgsfdgsdsadsadsadsdsadsadsadsadsadasfbfasefebsabfasbfabdsfsfbebaesbfasefbasebfaesfbaesfesbfaesfbasdsadasdadasdsadasafghnvghtoolooooongnaaaaaaameeeefghjkcxdftyhfghjkcxdfghjkcxhsshfgvhjvgjhgjhgjhgjhgj';
+        $this->assertFalse($ementa->validate(['Lanche2']));
+        $ementa->Lanche2 = 'Lanche2';
+        $this->assertTrue($ementa->validate(['Lanche2']));
+
+        $ementa->Jantar = null;
+        $this->assertFalse($ementa->validate(['Jantar']));
+        $ementa->Jantar = 'fghnvghtoolooooongndsdsadsadsadsadsadsadsadsazdsfsdfdsgfdgsgsbrsgbsbgsfersgsfdgsdsadsadsadsdsadsadsadsadsadasfbfasefebsabfasbfabdsfsfbebaesbfasefbasebfaesfbaesfesbfaesfbasdsadasdadasdsadasafghnvghtoolooooongnaaaaaaameeeefghjkcxdftyhfghjkcxdfghjkcxhsshfgvhjvgjhgjhgjhgjhgj';
+        $this->assertFalse($ementa->validate(['Jantar']));
+        $ementa->Jantar = 'Jantar';
+        $this->assertTrue($ementa->validate(['Jantar']));
+    }
+
+    function testSavingUser()
+    {
+
+        $ementa = new Ementa();
+        $ementa->PequenoAlmoco = 'PequenoAlmoco';
+        $ementa->Almoco = 'Almoco';
+        $ementa->Lanche1 = 'Lanche1';
+        $ementa->Lanche2 = 'Lanche2';
+        $ementa->Jantar = 'Jantar';
+
+        $ementa->save();
+        $this->tester->seeInDatabase('ementa', ['PequenoAlmoco' => 'PequenoAlmoco']);
+    }
+
+    
+    function testNameCanBeChanged()
+    {
+        $id = $this->tester->grabRecord('common\models\Ementa', ['PequenoAlmoco' => 'PequenoAlmoco']);
+
+        $ementa = Ementa::findOne($id);
+        $ementa->PequenoAlmoco = ('PequenoAlmocoTeste');
+        $ementa->save();
+
+        $this->tester->seeRecord('common\models\Ementa', ['PequenoAlmoco' => 'PequenoAlmocoTeste']);
+        $this->tester->dontSeeRecord('common\models\Ementa', ['PequenoAlmoco' => 'PequenoAlmoco']);
+    }
+    
+
+    function testUserDeleted()
+    {
+        $id = $this->tester->grabRecord('common\models\Ementa', ['PequenoAlmoco' => 'PequenoAlmocoTeste']);
+
+        $ementa = Ementa::findOne($id);
+        $ementa->delete();
+
+        $this->tester->dontSeeRecord('common\models\Ementa', ['PequenoAlmoco' => 'PequenoAlmocoTeste']);
+    }
+}
