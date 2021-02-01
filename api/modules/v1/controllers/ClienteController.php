8f5b718f51a7de680b058fd193452833a4d5d36a
Jason Mendes <jason_mendes_1998@hotmail.com>
2021-02-01 14:39:22 +0000

    API - cliente/get

--- api/modules/v1/controllers/ClienteController.php
+++ api/modules/v1/controllers/ClienteController.php
@@ -3,6 +3,7 @@
 namespace api\modules\v1\controllers;
 
 use common\models\Cliente;
+use Yii;
 use yii\rest\ActiveController;
 use yii\web\Response;
 
@@ -84,5 +85,70 @@ class ClienteController extends ActiveController
         $models = new $this->modelClass;
         $model = $models::findOne($id);
         return $model->massa_gorda;
+    }
+
+    public function actionGet($id)
+    {
+        $user = new $this->modelClass;
+        $userRecord = $user::find()->where("User_ID=" . $id)->one();
+        $cliente = array();
+
+        array_push(
+            $cliente,
+            [
+                "UserUsername" => $userRecord->user->username,
+                "ClientePrimeiroNome" => $userRecord->primeiroNome,
+                "ClienteApelido" => $userRecord->apelido,
+                "ClienteDataNasc" => $userRecord->dt_nascimento,
+                "ClienteSexo" => $userRecord->sexo,
+                "ClienteAvatar" => $userRecord->avatar,
+                "ClienteNumTele" => $userRecord->num_tele,
+                "ClienteNIF" => $userRecord->nif,
+                "ClienteAltura" => $userRecord->altura,
+                "ClientePeso" => $userRecord->peso,
+                "ClienteMassaMuscular" => $userRecord->massa_muscular,
+                "ClienteMassaGorda" => $userRecord->massa_gorda,
+                "UserEmail" => $userRecord->user->email,
+            ]
+        );
+        return $cliente;
+    }
+
+    public function actionEdit()
+    {
+        $request = Yii::$app->request;
+
+        $idUser = $request->get('IdUser');
+        $user = new $this->modelClass;
+        $cliente = new $this->modelCliente;
+        $userRecord = $user::find()->where("Id=" . '\'' . $idUser . '\'')->one();
+        $clienteRecord = $cliente::find()->where("Id=" . '\'' . $userRecord->cliente->Id . '\'')->one();
+        
+        $clienteNome = $request->get('ClientePrimeiroNome');
+        $clienteApelido = $request->get('ClienteApelido');
+        $clienteDataNasc = $request->get('ClienteDataNasc');
+        $clienteSexo = $request->get('ClienteSexo');
+        $clienteNumTele = $request->get('ClienteNumTele');
+        $clienteNIF = $request->get('ClienteNIF');
+        $clienteAltura = $request->get('ClienteAltura');
+        $clientePeso = $request->get('ClientePeso');
+        $clienteMassaMuscular = $request->get('ClienteMassaMuscular');
+        $clienteMassaGorda = $request->get('ClienteMassaGorda');
+
+        $clienteRecord->primeiroNome = $clienteNome;
+        $clienteRecord->apelido = $clienteApelido;
+        $clienteRecord->dt_nascimento = $clienteDataNasc;
+        $clienteRecord->sexo = $clienteSexo;
+        $clienteRecord->num_tele = $clienteNumTele;
+        $clienteRecord->nif = $clienteNIF;
+        $clienteRecord->altura = $clienteAltura;
+        $clienteRecord->peso = $clientePeso;
+        $clienteRecord->massa_muscular = $clienteMassaMuscular;
+        $clienteRecord->massa_gorda = $clienteMassaGorda;
+
+        $userRecord->save();
+        $clienteRecord->save(false);
+
+        return $clienteRecord->Id;
     }
 }
